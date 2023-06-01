1af05cb8b5c5e3101347eb6ac42c8870c9a38c15
Syler <syler@syler.xyz>
2023-06-01 17:30:32 +0200

    Minimal setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reflect-utils',
+    version='0.0.0',
+    description='Utilities for processing and analysis of Reflect data',
+    url='https://github.com/yourusername/reflect-utils',
+    packages=find_packages(),
+)
