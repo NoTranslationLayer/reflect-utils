f0458b47a1fc3373453160e64a7f49d7b2690294
Syler <syler@syler.xyz>
2023-06-01 17:50:42 +0200

    Use setup.cfg with minimal setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='reflect-utils',
-    version='0.0.0',
-    description='Utilities for processing and analysis of Reflect data',
-    url='https://github.com/yourusername/reflect-utils',
-    packages=find_packages(),
-)
+if __name__ == "__main__":
+    setup()
