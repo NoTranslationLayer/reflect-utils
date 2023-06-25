6c3edf9b2bac072b244af1fc30f5ff8cf780b84d
Syler <syler@syler.xyz>
2023-06-25 18:11:28 -0400

    Update json_to_csv script to load YAML with parsing options

--- scripts/json_to_csv.py
+++ scripts/json_to_csv.py
@@ -9,27 +9,35 @@ def main():
     Each reflection in the JSON file will be parsed into a separate CSV file.
     The CSV files will be saved in the specified output directory.
     An optional list of reflection names can be provided to filter which 
-    reflections to save.
+    reflections to save. Additionally, parsing options can be loaded from a 
+    YAML file.
 
     Command-line arguments:
     - json_path: Path to the JSON reflections file.
     - output_dir: Output directory for CSV files.
     - reflections: Optional list of reflection names to save. If not provided, 
         all reflections will be saved.
-    - default_value: Optional default value for unrecorded metrics.
+    - options_file: Optional YAML file with parsing options.
     """
     parser = argparse.ArgumentParser(description='Parse JSON reflections and save to CSV.')
     parser.add_argument('json_path', type=str, help='Path to the JSON reflections file.')
     parser.add_argument('output_dir', type=str, help='Output directory for CSV files.')
     parser.add_argument('-r', '--reflections', nargs='*', help='Optional list of reflection names to save.')
-    parser.add_argument('-d', '--default_value', type=str, default=None, help='Default value for unrecorded metrics.')
+    parser.add_argument('-o', '--options_file', type=str, default=None, help='YAML file with parsing options.')
 
     args = parser.parse_args()
 
     with open(args.json_path, 'r') as f:
         json_string = f.read()
 
-    reflections_map = conv.parse_json(json_string, default_value=args.default_value)
+    if args.options_file:
+        with open(args.options_file, 'r') as f:
+            options_yaml = f.read()
+        options = conv.load_parsing_options_from_yaml(options_yaml)
+    else:
+        options = conv.ParsingOptions()
+
+    reflections_map = conv.parse_json(json_string, options=options)
     conv.save_dataframes_to_csv(reflections_map, args.output_dir, args.reflections)
 
 if __name__ == "__main__":
