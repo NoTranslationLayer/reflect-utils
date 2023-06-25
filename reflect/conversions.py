7ec72c15487141012478977f210ec9b82c472baa
Syler <syler@syler.xyz>
2023-06-25 18:04:00 -0400

    ParsingOptions class and test for parse_metric_value function

--- reflect/conversions.py
+++ reflect/conversions.py
@@ -1,21 +1,146 @@
 import pandas as pd
 import json
 import os
+import yaml
 from datetime import datetime
 from dateutil import tz
 from typing import Dict, List, Optional, Tuple, Any
 
 
+class ParsingOptions:
+    """
+    This class holds the default values for different metric kinds. It supports
+    three types of default values:
+    1. The value to use before the first occurrence of a metric.
+    2. The value to use if a metric is not recorded.
+    3. The value to use if a metric has been removed from subsequent reflection
+       instances.
+    These defaults can be updated using a YAML file.
+    """
+
+    def __init__(self):
+        """
+        Initializes the default, pre_metric, and post_metric values for
+        different metric kinds.
+        """
+        self.defaults = {
+            "string": "",
+            "choice": None,
+            "bool": False,
+            "unit": 0,
+            "rating": None,
+            "scalar": None,
+        }
+        self.pre_metric_defaults = {
+            "string": "",
+            "choice": None,
+            "bool": False,
+            "unit": 0,
+            "rating": None,
+            "scalar": None,
+        }
+        self.post_metric_defaults = {
+            "string": "",
+            "choice": None,
+            "bool": False,
+            "unit": 0,
+            "rating": None,
+            "scalar": None,
+        }
+
+    def load_from_yaml(self, yaml_file: str):
+        """
+        Load the default values from a YAML file. The file should contain
+        dictionaries for 'defaults', 'pre_metric_defaults', and
+        'post_metric_defaults' where the keys are metric kinds and the values
+        are the default values.
+        """
+        with open(yaml_file, "r") as file:
+            new_defaults = yaml.safe_load(file)
+
+        self.defaults.update(new_defaults.get("defaults", {}))
+        self.pre_metric_defaults.update(
+            new_defaults.get("pre_metric_defaults", {})
+        )
+        self.post_metric_defaults.update(
+            new_defaults.get("post_metric_defaults", {})
+        )
+
+    def get_default_value(self, metric_kind: str):
+        """Returns the default value for a given metric kind."""
+        return self.defaults.get(metric_kind)
+
+    def get_pre_metric_default(self, metric_kind: str):
+        """
+        Returns the default value for a given metric kind before its first
+        occurrence.
+        """
+        return self.pre_metric_defaults.get(metric_kind)
+
+    def get_post_metric_default(self, metric_kind: str):
+        """
+        Returns the default value for a given metric kind after it has been
+        removed from subsequent reflection instances.
+        """
+        return self.post_metric_defaults.get(metric_kind)
+
+
+def parse_metric_value(metric: Dict[str, Any]) -> Optional[Any]:
+    """
+    This function parses the value of a given metric based on its kind.
+
+    Args:
+        metric (Dict[str, Any]): A dictionary that contains information about
+        a metric, including its kind and value.
+
+    Returns:
+        (str, str, Any): A tuple with the name, kind, and the parsed value of
+        the metric, if it exists and is recorded. Otherwise, returns None.
+
+    Raises:
+        KeyError: If "kind" key does not exist in the metric or its value is
+        empty.
+    """
+    if "kind" not in metric or not metric["kind"]:
+        raise KeyError(f'"kind" not found in metric: {metric}')
+    metric_kind = list(metric["kind"].keys())[0]
+    metric_content = metric["kind"][metric_kind].get("_0")
+
+    if metric_content is not None:
+        try:
+            metric_name = metric_content["name"]
+            if metric_kind == "string":
+                value = metric_content["string"]
+            elif metric_kind == "choice":
+                value = metric_content["choice"]
+            elif metric_kind == "bool":
+                value = metric_content["bool"]
+            elif metric_kind == "unit":
+                value = metric_content["value"]
+            elif metric_kind == "rating":
+                value = metric_content["score"]
+            elif metric_kind == "scalar":
+                value = metric_content["scalar"]
+        except KeyError:
+            return None
+    if "recorded" in metric and not metric["recorded"]:
+        return None
+    return metric_name, metric_kind, value
+
+
 def parse_metrics(
-    metrics: List[Dict[str, Any]], default_value: Any = None
+    metrics: List[Dict[str, Any]],
+    parsing_options: ParsingOptions,
+    existing_columns: List[str],
 ) -> Dict[str, Any]:
     """
     Parse the metrics from a reflection instance into a dictionary.
 
     Args:
         metrics (list): A list of metrics in a reflection instance.
-        default_value (Any, optional): The default value to use if a metric is
-            not recorded. Defaults to None.
+        parsing_options (ParsingOptions): The default value options.
+        existing_columns (List[str]): List of existing columns in the DataFrame
+            for the reflection.
 
     Returns:
         dict: A dictionary with metric names as keys and metric values as values.
@@ -23,37 +148,33 @@ def parse_metrics(
     metric_dict = {}
 
     for metric in metrics:
-        metric_kind = list(metric["kind"].keys())[0]
-        # The "_0" string is an artifact of how Swift's JSONEncoder handles
-        # encoding of enum cases with associated values. See Apple
-        # documentation of JSONencoder in Swift:
-        # https://developer.apple.com/documentation/foundation/jsonencoder
-        metric_val = metric["kind"][metric_kind].get("_0")
+        result = parse_metric_value(metric)
 
-        if metric_val is not None:
-            try:
-                if metric_kind == "string":
-                    metric_dict[metric_val["name"]] = metric_val["string"]
-                elif metric_kind == "choice":
-                    metric_dict[metric_val["name"]] = metric_val["choice"]
-                elif metric_kind == "bool":
-                    metric_dict[metric_val["name"]] = metric_val["bool"]
-                elif metric_kind == "unit":
-                    metric_dict[metric_val["name"]] = metric_val["value"]
-                elif metric_kind == "rating":
-                    metric_dict[metric_val["name"]] = metric_val["score"]
-                elif metric_kind == "scalar":
-                    metric_dict[metric_val["name"]] = metric_val["scalar"]
-            except KeyError:
-                metric_dict[metric_val["name"]] = default_value
-        elif "recorded" in metric and not metric["recorded"]:
-            metric_dict[metric_val["name"]] = default_value
+        if result is not None:
+            metric_name, metric_kind, metric_val = result
+            metric_dict[metric_name] = metric_val
+        elif metric_name in existing_columns:
+            metric_dict[metric_name] = parsing_options.get_post_metric_default(
+                metric_kind
+            )
+        else:
+            metric_dict[metric_name] = parsing_options.get_default_value(
+                metric_kind
+            )
+
+    for column in existing_columns:
+        if column not in metric_dict:
+            metric_dict[column] = parsing_options.get_pre_metric_default(
+                metric_kind
+            )
 
     return metric_dict
 
 
 def parse_reflection(
-    reflection: Dict, default_value=None
+    reflection: Dict,
+    parsing_options: ParsingOptions,
+    existing_df: pd.DataFrame = None,
 ) -> Tuple[str, pd.DataFrame]:
     """
     Parses an individual reflection instance into a DataFrame, which is
@@ -61,8 +182,9 @@ def parse_reflection(
 
     Args:
         reflection (dict): A dictionary representing a reflection.
-        default_value (Any, optional): The default value to use if a metric is
-            not recorded. Defaults to None.
+        parsing_options (ParsingOptions): The default value options.
+        existing_df (DataFrame, optional): The existing DataFrame for the
+            reflection. Defaults to None.
 
     Returns:
         str: The name of the reflection.
@@ -85,26 +207,37 @@ def parse_reflection(
         .strftime("%Y-%m-%d %H:%M:%S")
     )
 
-    reflection_row = parse_metrics(reflection["metrics"], default_value)
+    existing_columns = (
+        existing_df.columns.tolist() if existing_df is not None else []
+    )
+    reflection_row = parse_metrics(
+        reflection["metrics"], parsing_options, existing_columns
+    )
     reflection_row["Timestamp"] = timestamp
     reflection_row["Date"] = date
     reflection_row["ID"] = reflection["id"]
     reflection_row["Notes"] = reflection.get("notes")
 
+    # Use post_metric_default for any columns in existing_df not present in this reflection instance
+    if existing_df is not None:
+        for column in existing_df.columns:
+            if column not in reflection_row:
+                reflection_row[
+                    column
+                ] = parsing_options.get_post_metric_default(column)
+
     return name, pd.DataFrame([reflection_row])
 
-# TODO(@syler): optons for making default value metric-specific or metric type
-# specific
+
 def parse_json(
-    json_string: str, default_value=None
+    json_string: str, parsing_options: ParsingOptions
 ) -> Dict[str, pd.DataFrame]:
     """
     Parses a JSON string into a map from reflection names to DataFrames.
 
     Args:
         json_string (str): A JSON string.
-        default_value (Any, optional): The default value to use if a metric is
-            not recorded. Defaults to None.
+        parsing_options (ParsingOptions): The default value options.
 
     Returns:
         dict: A map where the keys are the reflection names and the values are
@@ -114,7 +247,8 @@ def parse_json(
     data = json.loads(json_string)
     reflections_map = {}
     for reflection in data:
-        name, df = parse_reflection(reflection, default_value)
+        existing_df = reflections_map.get(reflection["name"])
+        name, df = parse_reflection(reflection, parsing_options, existing_df)
         if name in reflections_map:
             reflections_map[name] = pd.concat(
                 [df, reflections_map[name]], ignore_index=True
--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -8,7 +8,67 @@ from pandas.testing import assert_frame_equal
 from reflect import conversions as conv
 
 
-class TestParsingFunctions(unittest.TestCase):
+
+
+class TestParsingMetricValue(unittest.TestCase):
+    """
+    Tests the `parse_metric_value` function's ability to correctly extract the 
+    metric name, kind, and value from the provided metric dictionary. It also 
+    tests the function's behavior when the metric "kind" is missing or when the 
+    metric is present but is not recorded.
+    """
+
+    def setUp(self):
+        self.metric_data = {
+            "kind": {
+                "bool": {
+                    "_0": {
+                        "name": "No Gi",
+                        "bool": False
+                    }
+                }
+            },
+            "id": "994FAC7F-FA9D-4B5E-9896-F72165F72A6C",
+            "group": "Info"
+        }
+        self.metric_no_kind = {
+            "_0": {
+                "name": "No Gi",
+                "bool": False
+            },
+            "id": "994FAC7F-FA9D-4B5E-9896-F72165F72A6C",
+            "group": "Info"
+        }
+        self.metric_not_recorded = {
+            "kind": {
+                "bool": {
+                    "_0": {
+                        "name": "No Gi",
+                        "bool": False
+                    }
+                }
+            },
+            "id": "994FAC7F-FA9D-4B5E-9896-F72165F72A6C",
+            "group": "Info",
+            "recorded": False
+        }
+
+    def test_parse_metric_value(self):
+        name, kind, value = conv.parse_metric_value(self.metric_data)
+        self.assertEqual(name, 'No Gi')
+        self.assertEqual(kind, 'bool')
+        self.assertEqual(value, False)
+
+    def test_parse_metric_no_kind(self):
+        with self.assertRaises(KeyError):
+            conv.parse_metric_value(self.metric_no_kind)
+
+    def test_parse_metric_not_recorded(self):
+        result = conv.parse_metric_value(self.metric_not_recorded)
+        self.assertEqual(result, None)
+
+
+class TestJsonToCsvParsing(unittest.TestCase):
     def setUp(self):
         self.json_string = """
         [
